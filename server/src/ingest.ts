a78f36c60bb8fef03eb3295f184b027a98dcf41e
Divyansh Sharma <divyanshsharma4507@gmail.com>
2025-07-21 20:50:59 +0530

    Add Google Generative AI integration and new dependencies
    - Updated package.json and package-lock.json to include @langchain/google-genai.
    - Enhanced ingest.ts to utilize GoogleGenerativeAIEmbeddings for embedding PDFs.
    - Configured dotenv for environment variable management.

--- server/src/ingest.ts
+++ server/src/ingest.ts
@@ -1,22 +1,34 @@
 import fs from 'fs';
 import pdfParse from 'pdf-parse';
-import {RecursiveCharacterTextSplitter} from 'langchain/text_splitter'
+import { RecursiveCharacterTextSplitter } from 'langchain/text_splitter';
+import { MemoryVectorStore } from 'langchain/vectorstores/memory';
+import { GoogleGenerativeAIEmbeddings } from '@langchain/google-genai';
+import dotenv from 'dotenv';
 
-export const loadPdf = async (pdfPath: string) : Promise<string> => {
+dotenv.config();
+
+export const loadPdf = async (pdfPath: string): Promise<string> => {
     const dataBuffer = fs.readFileSync(pdfPath);
     const data = await pdfParse(dataBuffer);
-    
     return data.text;
-}
+};
 
-
-export const embedPdf = async()=>{
+export const embedPdf = async () => {
     const text = await loadPdf('src/data/hireDivyanshSharma.pdf');
     const textSplitter = new RecursiveCharacterTextSplitter({
         chunkSize: 1000,
         chunkOverlap: 100,
     });
     const docs = await textSplitter.createDocuments([text]);
-    return docs;
-}
+
+   
+    const embeddings = new GoogleGenerativeAIEmbeddings({
+        apiKey: process.env.GOOGLE_API_KEY,
+        model: 'models/embedding-001', 
+    });
+
+    const vectorStore = await MemoryVectorStore.fromDocuments(docs, embeddings);
+
+};
+
 
